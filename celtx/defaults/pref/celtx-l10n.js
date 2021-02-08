b76da84d827de71bf8854e04362267d4742a60a0
Florian-COLLIN <73430474+Florian-COLLIN@users.noreply.github.com>
2021-02-08 12:36:01 +0100

    Add all files

---
+++ celtx/defaults/pref/celtx-l10n.js
@@ -0,0 +1,19 @@
+//@line 35 "/home/tony/Development/celtx/mozilla/celtx/locales/en-US/celtx-l10n.js"
+
+//@line 37 "/home/tony/Development/celtx/mozilla/celtx/locales/en-US/celtx-l10n.js"
+
+pref("general.useragent.locale", "en-US");
+pref("spellchecker.dictionary", "en-US");
+
+pref("celtx.scripteditor.intexts", "INT,EXT");
+pref("celtx.scripteditor.daynights", "DAY,NIGHT,MORNING,NOON,AFTERNOON,EVENING,MIDNIGHT,DUSK,DAWN,SUNRISE,SUNSET,CONTINUOUS,LATER,MOMENTS LATER");
+pref("celtx.scripteditor.shots","WIDE:,VERY WIDE SHOT:,EXTREME WIDE SHOT:,MASTER:,MEDIUM:,CLOSE UP:,MEDIUM CLOSE UP:,EXTREME CLOSE UP:,CUTAWAY:,CUT-IN:,ZOOM IN:,PAN:,TWO-SHOT:,OVER THE SHOULDER:,POV:,MONTAGE:,CGI:,WEATHER SHOT:");
+pref("celtx.tag.white", "Plot A");
+pref("celtx.tag.grey", "Plot B");
+pref("celtx.tag.blue", "Plot C");
+pref("celtx.tag.green", "Plot D");
+pref("celtx.tag.yellow", "Plot E");
+pref("celtx.tag.orange", "Plot F");
+pref("celtx.tag.pink", "Plot G");
+pref("celtx.tag.red", "Plot H");
+pref("celtx.tag.purple", "Plot I");
---
+++ celtx/defaults/pref/channel-prefs.js
@@ -0,0 +1,2 @@
+//@line 2 "/home/tony/Development/celtx/mozilla/celtx/app/profile/channel-prefs.js"
+pref("app.update.channel", "default");
